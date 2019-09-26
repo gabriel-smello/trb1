f5b32269a6c51474f8581adb6624630ff3dc3fa8
igortaquary <55813558+igortaquary@users.noreply.github.com>
2019-09-26 14:47:50 -0300

    Create teste_dominio.hpp

---
+++ teste_dominio.hpp
@@ -0,0 +1,29 @@
+#ifndef TESTE_DOMINIO_H
+#define TESTE_DOMINIO_H
+#include "dominios.hpp"
+
+using namespace std;
+
+class TUVagas {
+private:
+    const static int VALOR_VALIDO   = 0;
+    const static int VALOR_INVALIDO = 5;
+
+    Vagas *codigo;         // Referência para o objeto a ser testado.
+
+    int estado;             // Estado do teste.
+
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+
+public:
+    const static int SUCESSO =  0;
+    const static int FALHA   = -1;
+
+    int run();
+};
+
+
+#endif //TESTE_DOMINIO_H
