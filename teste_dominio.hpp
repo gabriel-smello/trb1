174a9d89f286ccd97e74a0b705059f1a2948e7bf
igortaquary <55813558+igortaquary@users.noreply.github.com>
2019-09-26 16:38:08 -0300

    Update teste_dominio.hpp

--- teste_dominio.hpp
+++ teste_dominio.hpp
@@ -4,6 +4,27 @@
 
 using namespace std;
 
+class TUSenha {
+private:
+    const string VALOR_VALIDO   = "$Enh4";
+    const string VALOR_INVALIDO = "agora";
+
+    Senha *codigo;         // Referência para o objeto a ser testado.
+
+    int estado;             // Estado do teste.
+
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+
+public:
+    const static int SUCESSO =  0;
+    const static int FALHA   = -1;
+
+    int run();
+};
+
 class TUVagas {
 private:
     const static int VALOR_VALIDO   = 0;
