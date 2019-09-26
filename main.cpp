be02a2f859317d78a27175588553fd5e38592735
igortaquary <55813558+igortaquary@users.noreply.github.com>
2019-09-26 14:44:46 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "dominios.hpp"
+#include "teste_dominio.hpp"
+using namespace std;
+
+int main()
+{
+    //Aqui vai os testes de cada um dos dominios
+    
+    TUVagas TesteVaga;
+    TesteVaga.run();
+
+    return 0;
+}
