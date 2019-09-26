1f75d9a9baf670011bc856bcc72044f024e8ccaa
igortaquary <55813558+igortaquary@users.noreply.github.com>
2019-09-26 16:39:07 -0300

    Update teste_dominio.cpp

--- teste_dominio.cpp
+++ teste_dominio.cpp
@@ -1,14 +1,49 @@
 #include "teste_dominio.hpp"
 
-//Definições de constantes.
+// Definições de métodos da classe TUSenha
+void TUSenha::setUp(){
+    codigo = new Senha();
+    estado = SUCESSO;
+}
 
-const int TUVagas::SUCESSO;
-const int TUVagas::FALHA;
-const int TUVagas::VALOR_VALIDO;
-const int TUVagas::VALOR_INVALIDO;
+void TUSenha::tearDown(){
+    delete codigo;
+}
+
+void TUSenha::testarCenarioSucesso(){
+    try{
+        codigo->setSenha(VALOR_VALIDO);
+        if (codigo->getSenha() != VALOR_VALIDO){
+            cout << "Falha no valor valido de Senha no get" << endl;
+            estado = FALHA;
+        }
+    }
+    catch(invalid_argument){
+        cout << "Falha no valor valido de Senha no set" << endl;
+        estado = FALHA;
+    }
+}
+
+void TUSenha::testarCenarioFalha(){
+    try{
+        codigo->setSenha(VALOR_INVALIDO);
+        cout << "Falha no valor invalido de Senha" << endl;
+        estado = FALHA;
+    }
+    catch(invalid_argument){
+        return;
+    }
+}
+
+int TUSenha::run(){
+    setUp();
+    testarCenarioSucesso();
+    testarCenarioFalha();
+    tearDown();
+    return estado;
+}
 
 // Definições de métodos da classe TUVagas
-
 void TUVagas::setUp(){
     codigo = new Vagas();
     estado = SUCESSO;
@@ -21,10 +56,13 @@ void TUVagas::tearDown(){
 void TUVagas::testarCenarioSucesso(){
     try{
         codigo->setVagas(VALOR_VALIDO);
-        if (codigo->getVagas() != VALOR_VALIDO)
+        if (codigo->getVagas() != VALOR_VALIDO){
+            cout << "Falha no valor valido de vagas no get" << endl;
             estado = FALHA;
+        }
     }
-    catch(invalid_argument){
+    catch(invalid_argument (excecao)){
+        cout << "Falha no valor valido de vagas no set" << endl;
         estado = FALHA;
     }
 }
@@ -32,6 +70,7 @@ void TUVagas::testarCenarioSucesso(){
 void TUVagas::testarCenarioFalha(){
     try{
         codigo->setVagas(VALOR_INVALIDO);
+        cout << "Falha no valor invalido de vagas" << endl;
         estado = FALHA;
     }
     catch(invalid_argument){
