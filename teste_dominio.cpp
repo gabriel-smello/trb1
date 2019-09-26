ed6315df1c0c14d6e565ec5dde06a6eb4415c614
igortaquary <55813558+igortaquary@users.noreply.github.com>
2019-09-26 20:00:17 -0300

    Update teste_dominio.cpp

--- teste_dominio.cpp
+++ teste_dominio.cpp
@@ -1,5 +1,134 @@
 #include "teste_dominio.hpp"
 
+// Definições de métodos da classe TUNumeroConta
+void TUNumeroConta::setUp(){
+    codigo = new NumeroConta();
+    estado = SUCESSO;
+}
+
+void TUNumeroConta::tearDown(){
+    delete codigo;
+}
+
+void TUNumeroConta::testarCenarioSucesso(){
+    try{
+        codigo->setNumeroConta(VALOR_VALIDO);
+        if (codigo->getNumeroConta() != VALOR_VALIDO){
+            cout << "Falha no valor valido de NumeroConta no get" << endl;
+            estado = FALHA;
+        }
+    }
+    catch(invalid_argument){
+        cout << "Falha no valor valido de NumeroConta no set" << endl;
+        estado = FALHA;
+    }
+}
+
+void TUNumeroConta::testarCenarioFalha(){
+    try{
+        codigo->setNumeroConta(VALOR_INVALIDO);
+        cout << "Falha no valor invalido de NumeroConta" << endl;
+        estado = FALHA;
+    }
+    catch(invalid_argument){
+        return;
+    }
+}
+
+int TUNumeroConta::run(){
+    setUp();
+    testarCenarioSucesso();
+    testarCenarioFalha();
+    tearDown();
+    return estado;
+}
+
+// Definições de métodos da classe TUPreco
+void TUPreco::setUp(){
+    codigo = new Preco();
+    estado = SUCESSO;
+}
+
+void TUPreco::tearDown(){
+    delete codigo;
+}
+
+void TUPreco::testarCenarioSucesso(){
+    try{
+        codigo->setPreco(VALOR_VALIDO);
+        if (codigo->getPreco() != VALOR_VALIDO){
+            cout << "Falha no valor valido de Preco no get" << endl;
+            estado = FALHA;
+        }
+    }
+    catch(invalid_argument){
+        cout << "Falha no valor valido de Preco no set" << endl;
+        estado = FALHA;
+    }
+}
+
+void TUPreco::testarCenarioFalha(){
+    try{
+        codigo->setPreco(VALOR_INVALIDO);
+        cout << "Falha no valor invalido de Preco" << endl;
+        estado = FALHA;
+    }
+    catch(invalid_argument){
+        return;
+    }
+}
+
+int TUPreco::run(){
+    setUp();
+    testarCenarioSucesso();
+    testarCenarioFalha();
+    tearDown();
+    return estado;
+}
+
+// Definições de métodos da classe TUTelefone
+void TUTelefone::setUp(){
+    codigo = new Telefone();
+    estado = SUCESSO;
+}
+
+void TUTelefone::tearDown(){
+    delete codigo;
+}
+
+void TUTelefone::testarCenarioSucesso(){
+    try{
+        codigo->setTelefone(VALOR_VALIDO);
+        if (codigo->getTelefone() != VALOR_VALIDO){
+            cout << "Falha no valor valido de Telefone no get" << endl;
+            estado = FALHA;
+        }
+    }
+    catch(invalid_argument){
+        cout << "Falha no valor valido de Telefone no set" << endl;
+        estado = FALHA;
+    }
+}
+
+void TUTelefone::testarCenarioFalha(){
+    try{
+        codigo->setTelefone(VALOR_INVALIDO);
+        cout << "Falha no valor invalido de Telefone" << endl;
+        estado = FALHA;
+    }
+    catch(invalid_argument){
+        return;
+    }
+}
+
+int TUTelefone::run(){
+    setUp();
+    testarCenarioSucesso();
+    testarCenarioFalha();
+    tearDown();
+    return estado;
+}
+
 // Definições de métodos da classe TUSenha
 void TUSenha::setUp(){
     codigo = new Senha();
