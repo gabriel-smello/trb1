52cb12288ac4e8642d245827e7294060a4b876a7
igortaquary <55813558+igortaquary@users.noreply.github.com>
2019-09-27 13:02:39 -0300

    main.cpp ATUALIZADA

--- main.cpp
+++ main.cpp
@@ -1,16 +1,52 @@
 #include <iostream>
 #include "dominios.hpp"
 #include "teste_dominio.hpp"
+
 using namespace std;
 
 int main()
 {
-    //Aqui vai os testes de cada um dos dominios
-    TUSenha TesteSenha;
-    TUVagas TesteVaga;
-    
-    
-    cout << "Senha: " << TesteSenha.run() << "\n\n";
+    cout << "Testando..." << endl;
+    cout << "0 = SUCESSO   ;   -1 = FALHA \n" << endl;
+
+    TUAssento       TesteAssento;
+    TUBagagem       TesteBagagem;
+    TUCodigoBanco   TesteCodigoBanco;
+    TUCodigoCarona  TesteCodigoCarona;
+    TUCodigoReserva TesteCodigoReserva;
+    TUCidade        TesteCidade;
+    TUCpf           TesteCpf;
+    TUData          TesteData;
+    TUDuracao       TesteDuracao;
+    TUEstado        TesteEstado;
+    TUEmail         TesteEmail;
+    TUNome          TesteNome;
+    TUNumeroAgencia TesteNumeroAgencia;
+    TUNumeroConta   TesteNumeroConta;
+    TUPreco         TestePreco;
+    TUTelefone      TesteTelefone;
+    TUSenha         TesteSenha;
+    TUVagas         TesteVaga;
+
+    cout << "Assento: " << TesteAssento.run() << "\n";
+    cout << "Bagagem: " << TesteBagagem.run() << "\n";
+    cout << "CodigoBanco: " << TesteCodigoBanco.run() << "\n";
+    cout << "CodigoCarona: " << TesteCodigoCarona.run() << "\n";
+    cout << "CodigoReserva: " << TesteCodigoReserva.run() << "\n";
+    cout << "Cidade: " << TesteCidade.run() << "\n";
+    cout << "Cpf: " << TesteCpf.run() << "\n";
+    cout << "Data: " << TesteData.run() << "\n";
+    cout << "Duracao: " << TesteDuracao.run() << "\n";
+    cout << "Estado: " << TesteEstado.run() << "\n";
+    cout << "Email: " << TesteEmail.run() << "\n";
+    cout << "Nome: " << TesteNome.run() << "\n";
+    cout << "NumeroAgencia: " << TesteNumeroAgencia.run() << "\n";
+    cout << "NumeroConta: " << TesteNumeroConta.run() << "\n";
+    cout << "Preco: " << TestePreco.run() << "\n";
+    cout << "Telefone: " << TesteTelefone.run() << "\n";
+    cout << "Senha: " << TesteSenha.run() << "\n";
     cout << "Vaga: " << TesteVaga.run() << "\n";
+
+    cout << "Fim do teste." << endl;
     return 0;
 }
