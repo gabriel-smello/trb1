c410244ad97e7051f635f42f49e3025b6e7f9c4f
igortaquary <55813558+igortaquary@users.noreply.github.com>
2019-09-27 13:03:45 -0300

    teste_dominio.hpp ATUALIZADA

--- teste_dominio.hpp
+++ teste_dominio.hpp
@@ -4,6 +4,237 @@
 
 using namespace std;
 
+class TUAssento {
+private:
+    const char VALOR_VALIDO   = 'D';
+    const char VALOR_INVALIDO = 'b';
+
+    Assento *codigo;         // Referência para o objeto a ser testado.
+
+    int estado;             // Estado do teste.
+
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+
+public:
+    const static int SUCESSO =  0;
+    const static int FALHA   = -1;
+
+    int run();
+};
+
+class TUBagagem {
+private:
+    const int VALOR_VALIDO   = 3;
+    const int VALOR_INVALIDO = 7;
+
+    Bagagem *codigo;         // Referência para o objeto a ser testado.
+
+    int estado;             // Estado do teste.
+
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+
+public:
+    const static int SUCESSO =  0;
+    const static int FALHA   = -1;
+
+    int run();
+};
+
+class TUCodigoBanco {
+private:
+    const string VALOR_VALIDO   = "876";
+    const string VALOR_INVALIDO = "-83";
+
+    CodigoBanco *codigo;
+
+    int estado;
+
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+
+public:
+    const static int SUCESSO =  0;
+    const static int FALHA   = -1;
+
+    int run();
+};
+
+class TUCodigoCarona {
+private:
+    const string VALOR_VALIDO   = "8762";
+    const string VALOR_INVALIDO = "10000";
+
+    CodigoCarona *codigo;
+
+    int estado;
+
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+
+public:
+    const static int SUCESSO =  0;
+    const static int FALHA   = -1;
+
+    int run();
+};
+
+class TUCodigoReserva {
+private:
+    const string VALOR_VALIDO   = "00000";
+    const string VALOR_INVALIDO = "30813299";
+
+    CodigoReserva *codigo;
+
+    int estado;
+
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+
+public:
+    const static int SUCESSO =  0;
+    const static int FALHA   = -1;
+
+    int run();
+};
+
+class TUCidade {
+private:
+    const string VALOR_VALIDO   = "Brasilia.";
+    const string VALOR_INVALIDO = "76354";
+
+    Cidade *codigo;
+
+    int estado;
+
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+
+public:
+    const static int SUCESSO =  0;
+    const static int FALHA   = -1;
+
+    int run();
+};
+
+class TUCpf {
+private:
+    const string VALOR_VALIDO   = "03399105185";
+    const string VALOR_INVALIDO = "22212342224";
+
+    Cpf *codigo;
+
+    int estado;
+
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+
+public:
+    const static int SUCESSO =  0;
+    const static int FALHA   = -1;
+
+    int run();
+};
+
+class TUData {
+private:
+    const string VALOR_VALIDO   = "01/10/2019";
+    const string VALOR_INVALIDO = "29/02/2019";
+
+    Data *codigo;
+
+    int estado;
+
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+
+public:
+    const static int SUCESSO =  0;
+    const static int FALHA   = -1;
+
+    int run();
+};
+
+class TUDuracao {
+private:
+    const int VALOR_VALIDO   = 12;
+    const int VALOR_INVALIDO = 88;
+
+    Duracao *codigo;
+
+    int estado;
+
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+
+public:
+    const static int SUCESSO =  0;
+    const static int FALHA   = -1;
+
+    int run();
+};
+
+class TUEstado {
+private:
+    const string VALOR_VALIDO   = "ba";
+    const string VALOR_INVALIDO = "fg";
+
+    Estado *codigo;
+
+    int estado;
+
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+
+public:
+    const static int SUCESSO =  0;
+    const static int FALHA   = -1;
+
+    int run();
+};
+
+class TUEmail {
+private:
+    const string VALOR_VALIDO   = "Julinho.2012@hotmail.com";
+    const string VALOR_INVALIDO = "Case#ior.@gmail.com";
+
+    Email *codigo;         // Referência para o objeto a ser testado.
+
+    int estado;             // Estado do teste.
+
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+
+public:
+    const static int SUCESSO =  0;
+    const static int FALHA   = -1;
+
+    int run();
+};
+
 class TUNome {
 private:
     const string VALOR_VALIDO   = "Igor Laranja.";
@@ -133,7 +364,7 @@ public:
 class TUVagas {
 private:
     const static int VALOR_VALIDO   = 0;
-    const static int VALOR_INVALIDO = 5;
+    const static int VALOR_INVALIDO = -3;
 
     Vagas *codigo;         // Referência para o objeto a ser testado.
 
@@ -150,6 +381,5 @@ public:
 
     int run();
 };
-
 
 #endif //TESTE_DOMINIO_H
