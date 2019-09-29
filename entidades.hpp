b85dc92c4ccef2ff9665dd25dba798f16d220ce2
gabriel-smello <42751354+gabriel-smello@users.noreply.github.com>
2019-09-29 15:50:58 -0300

    Update entidades.hpp

--- entidades.hpp
+++ entidades.hpp
@@ -110,6 +110,54 @@ public:
         Estado getEstadoDestino() const;
         ///Metodo que define o objeto EstadoDestino
         void setEstadoDestino(const Estado&);
+        ///Metodo que retorna o objeto de Data
+        Data getData() const;
+        ///Metodo que define o objeto de Data
+        void setData(const Data&);
+        ///Metodo que retorna o objeto de Duracao
+        Duracao getDuracao() const;
+        ///Metodo que define o objeto de Duracao
+        void setDuracao(const Duracao&);
+        ///Metodo que retorna o objeto de Vagas
+        Vagas getVagas() const;
+        ///Metodo que define o objeto de Vagas
+        void setVagas(const Vagas&);
+        ///Metodo que retorna o objeto de Preco
+        Preco getPreco() const;
+        ///Metodo que define o objeto de Preco
+        void setPreco(const Preco&);
+};
+/**
+* Classe entidade que usa os atributos:
+* CodigoBanco, NumeroAgencia e NumeroConta
+*/
+///Classe de entidade composta por outras classes, salva para Conta.
+class Conta {
+private:
+    CodigoBanco banco;
+    NumeroAgencia agencia;
+    NumeroConta numero;
+public:
+    Conta(){}
+    Conta(CodigoBanco, NumeroAgencia, NumeroConta);
+    ///Metodo que retorna o objeto de CodigoBanco
+    CodigoBanco getCodigoBanco() const;
+    ///Metodo que define o objeto de CodigoBanco
+    void setCodigoBanco(const CodigoBanco&);
+    ///Metodo que retorna o objeto de NumeroAgencia
+    NumeroAgencia getNumeroAgencia() const;
+    ///Metodo que define o objeto de NumeroAgencia
+    void setNumeroAgencia(const NumeroAgencia&);
+    ///Metodo que retorna o objeto de NumeroConta
+    NumeroConta getNumeroConta() const;
+    ///Metodo que define o objeto de NumeroConta
+    void setNumeroConta(const NumeroConta&);
+};
+
+
+
+#endif // ENTIDADE_H
+
         Data getData() const;
         void setData(const Data&);
         Duracao getDuracao() const;
